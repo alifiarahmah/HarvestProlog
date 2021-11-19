82332991cfe52fce045e21599749ef8cf5a16e9f
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-19 23:52:02 +0700

    add quit

--- src/misc.pl
+++ src/misc.pl
@@ -9,3 +9,12 @@ writeTitle :-
 	write('     |_||_\\__,_|_|  \\_/\\___/__/\\__|_| |_| \\___/_\\___/\\__, |'), nl,
 	write('                                                     |___/'), nl,
 	write('================================================================'), nl.
+
+quit :-
+	write('      ___________'), nl,
+	write('     /  __   __  \\'), nl,
+	write('    /  /  | |  \\  \\       You decided to go back to the city.'), nl,
+	write(' __/  /___| |___\\  \\__    See you later in the next adventure.'), nl,
+	write('|     __       __     |'), nl,
+	write('|____/  \\_____/  \\____|'), nl,
+	write('     \\__/     \\__/'), nl, halt.
