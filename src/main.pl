9b33dabe5d227b0033c4240ccfadfe89d279f85b
fernaldyl <16520098@std.stei.itb.ac.id>
2021-11-20 22:36:35 +0700

    edited main

--- src/main.pl
+++ src/main.pl
@@ -1,5 +1,7 @@
 /* DYNAMIC FACTS */
 :- dynamic(started/1).
+:- dynamic(time/1).
+:- dynamic(position/2).
 
 /* PROGRAM UTAMA */
 
@@ -15,11 +17,25 @@ startGame :-
 	X =:= 0,
 	retractall(started(_)),
 	asserta(started(1)),
+	initTime,
+	createRandomMap,
+	initPosition,
 	writeTitleV2, nl,
 	write('Harvest Star!!!'), nl, nl,
 	write('Let\'s play and pay our debts together!'), nl, nl,
 	writeCommandList, nl.
 
+/* initTime */
+initTime :- 
+	retractall(time(_)),
+	asserta(time(0.0)).
+
+/* initPosition */
+initPosition :- 
+	house(X,Y),
+	retractall(position(_,_)),
+	asserta(position(X,Y)).
+	
 /* start: Mulai game baru dan inisialisasi gold, exp, level */
 start :- 
 	started(X),
@@ -37,6 +53,82 @@ start :-
 map :- 
 	started(X),
 	X =:= 1,
-	createRandomMap,
 	displayMap(0).
 
+/* w: Bergerak ke utara 1 langkah */
+w :-
+	position(X,Y),
+	Y1 is Y - 1,
+	(	Y =:= 1 ->
+		write('Ada batas di utara, tidak bisa bergerak ke utara'), nl,
+		fail
+	;	isAtAir(X, Y1) ->
+		write('Ada air di utara, tidak bisa bergerak ke utara'), nl,
+		fail
+	;	time(T),
+		retractall(position(_,_)),
+		retractall(time(_)),
+		T1 is T + 0.5,
+		asserta(position(X, Y1)),
+		asserta(time(T1))
+	).
+
+/* s: Bergerak ke selatan 1 langkah */
+s :-
+	position(X,Y),
+	Y1 is Y + 1,
+	lebar(L),
+	(	Y =:= L ->
+		write('Ada batas di selatan, tidak bisa bergerak ke selatan'), nl,
+		fail
+	;	isAtAir(X, Y1) ->
+		write('Ada air di selatan, tidak bisa bergerak ke selatan'), nl,
+		fail
+	;	time(T),
+		retractall(position(_,_)),
+		retractall(time(_)),
+		T1 is T + 0.5,
+		asserta(position(X, Y1)),
+		asserta(time(T1))
+	).
+
+/* d: Bergerak ke timur 1 langkah */
+d :-
+	position(X,Y),
+	X1 is X + 1,
+	panjang(P),
+	(	X =:= P ->
+		write('Ada batas di timur, tidak bisa bergerak ke timur'), nl,
+		fail
+	;	isAtAir(X1, Y) ->
+		write('Ada air di timur, tidak bisa bergerak ke timur'), nl,
+		fail
+	;	time(T),
+		retractall(position(_,_)),
+		retractall(time(_)),
+		T1 is T + 0.5,
+		asserta(position(X1, Y)),
+		asserta(time(T1))
+	).
+
+/* a: Bergerak ke barat 1 langkah */
+a :-
+	position(X,Y),
+	X1 is X - 1,
+	(	X =:= 1 ->
+		write('Ada batas di barat, tidak bisa bergerak ke barat'), nl,
+		fail
+	;	isAtAir(X1, Y) ->
+		write('Ada air di barat, tidak bisa bergerak ke barat'), nl,
+		fail
+	;	time(T),
+		retractall(position(_,_)),
+		retractall(time(_)),
+		T1 is T + 0.5,
+		asserta(position(X1, Y)),
+		asserta(time(T1))
+	).
+
+outputPos :- position(X,Y), write(X), write(','), write(Y), nl.
+	
+	
