147400475f4bc2fbbb0e0103b3e1739c444db34c
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-22 15:15:31 +0700

    add nl in quit

--- src/misc.pl
+++ src/misc.pl
@@ -42,4 +42,4 @@ quit :-
 	write(' __/  /___| |___\\  \\__    See you later in the next adventure.'), nl,
 	write('|     __       __     |'), nl,
 	write('|____/  \\_____/  \\____|'), nl,
-	write('     \\__/     \\__/'), nl, halt.
+	write('     \\__/     \\__/'), nl, nl, halt.
