d4aff752546f2f59b2ec50c1470b2af8669cc135
alifiarahmah <alifiarahmah03@gmail.com>
2021-11-28 14:44:49 +0700

    changed initial equipment level in prepareJob

--- src/player.pl
+++ src/player.pl
@@ -25,13 +25,13 @@ initiated(0).
 /* prepareJob: Menyiapkan inventory sesuai job (WIP) */
 prepareJob :- 
 	job(X), X = 'Fisherman',
-	addEquipment(fishing_rod, 1, 1).
+	addEquipment(fishing_rod, 1, 0).
 prepareJob :- 
 	job(X), X = 'Farmer',
-	addEquipment(shovel, 1, 1).
+	addEquipment(shovel, 1, 0).
 prepareJob :- 
 	job(X), X = 'Rancher',
-	addEquipment(handcarts, 1, 1).
+	addEquipment(handcarts, 1, 0).
 
 /* initPlayer: Menyiapkan status awal player sebelum mulai bermain */
 initPlayer :- 
